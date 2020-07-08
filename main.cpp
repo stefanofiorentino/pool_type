2f7360421fe8d665f19206f221db8ac7d7b12b12
Fiorentino Ing. Stefano <stefano.fiore84@gmail.com>
2020-07-08 18:27:10 +0200

    adding publish functionality, but cannot get type inference to work
    Signed-off-by: Fiorentino Ing. Stefano <stefano.fiore84@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,56 +1,104 @@
 #include <iostream>
 #include <tuple>
+#include <list>
+#include <functional>
 
-template<typename E>
-struct Connection {
-};
+template<typename ...T>
+void pretty_print(T &&...t) {
+    std::puts(__PRETTY_FUNCTION__);
+}
 
-template<typename E>
-struct Listener {
-};
 
 template<typename T, typename ...Events>
 struct Emitter {
     template<typename E>
     struct Handler {
-        Connection<E> once(Listener<E> f) {
-            return Connection<E>{};
+        using Listener = std::function<void(E &, T &)>;
+        using Element = std::pair<bool, Listener>;
+        using ListenerList = std::list<Element>;
+        using Connection = typename ListenerList::iterator;
+
+        Connection once(Listener f) {
+            return onceL.emplace(onceL.cend(), false, std::move(f));
         }
+
+
+        void publish(E event, T &ref) {
+            ListenerList currentL;
+            onceL.swap(currentL);
+
+            auto func = [&event, &ref](auto &&element) {
+                return element.first ? void() : element.second(event, ref);
+            };
+
+            publishing = true;
+
+            std::for_each(currentL.rbegin(), currentL.rend(), func);
+
+            publishing = false;
+        }
+
+        bool empty() {
+            return true;
+        }
+    private:
+        bool publishing{false};
+        ListenerList onceL{};
     };
+
+    template<typename E>
+    struct Connection: private Handler<E>::Connection {
+        template<typename, typename ...> friend class Emitter;
+
+        Connection() = default;
+        Connection(const Connection &) = default;
+        Connection(Connection &&) = default;
+        ~Connection() = default;
+
+        Connection(typename Handler<E>::Connection conn)
+        : Handler<E>::Connection{std::move(conn)}
+        {}
+
+        Connection & operator=(const Connection &) = default;
+        Connection & operator=(Connection &&) = default;
+    };
+
+    template<typename E>
+    using Listener = typename Handler<E>::Listener;
 
     template<typename E>
     Connection<E> once(Listener<E> f) {
         return std::get<Handler<E> >(pools).once(std::move(f));
     }
 
-    std::tuple<Handler<Events>... > pools;
+    template<typename E>
+    void publish(E event) {
+        pretty_print(event);
+        std::get<Handler<E>>(pools).publish(std::move(event), *static_cast<T*>(this));
+    }
+
+    bool empty() const noexcept {
+        auto empty{true};
+        std::apply([&empty](auto&&... args) {((empty &= args.empty()), ...);}, pools);
+        return empty;
+    }
+
+    std::tuple<Handler<Events>...> pools;
 };
 
-template<typename T>
-struct Emitter<T> {
-};
-
-struct FakeHandle {
-};
 struct FakeEvent1 {
 };
 struct FakeEvent2 {
 };
 
-template<typename T>
-void pretty_print(T &&t) {
-    std::puts(__PRETTY_FUNCTION__);
-}
+struct FakeHandle : public Emitter<FakeHandle, FakeEvent1, FakeEvent2> {
+};
 
 int main() {
-    Emitter<FakeHandle> e0;
-
-    Emitter<FakeHandle, FakeEvent1> e1;
-    pretty_print(e1.once(Listener<FakeEvent1>{}));
-
     Emitter<FakeHandle, FakeEvent1, FakeEvent2> e2;
-    pretty_print(e2.once<FakeEvent1>(Listener<FakeEvent1>{}));
-    pretty_print(e2.once(Listener<FakeEvent2>{}));
-
+    auto connection = e2.once<FakeEvent2>([](FakeEvent2 &, FakeHandle &) {
+        std::puts(__PRETTY_FUNCTION__ );
+    });
+    e2.publish(FakeEvent2{});
     return 0;
 }
