49d51b3e43e1c3510ee62c384aabf3acd7a3f738
Fiorentino, Stefano <stefano.fiorentino@adesso.ch>
2022-02-24 11:49:52 +0100

    move to a e2e test

--- test/emitter.cpp
+++ test/emitter.cpp
@@ -9,34 +9,24 @@ struct FakeEmitter : public Emitter<FakeEmitter, FakeEvent> {
 
 struct FakeEmitterFixture : public ::testing::Test {
     FakeEmitterFixture() :
-            fakeHandle{std::make_shared<FakeEmitter>()} {
+            fakeEmitter{std::make_shared<FakeEmitter>()} {
     }
 
-    std::shared_ptr<FakeEmitter> fakeHandle;
+    std::shared_ptr<FakeEmitter> fakeEmitter;
 };
 
-TEST_F(FakeEmitterFixture, on_publish) {
+TEST_F(FakeEmitterFixture, e2e) {
     auto called{false};
-    fakeHandle->on([&called](const auto &, auto &) {
+    ASSERT_TRUE(fakeEmitter->empty());
+
+    fakeEmitter->on([&called](const auto &, auto &) {
         called = true;
     });
-    fakeHandle->publish(FakeEvent{});
+    ASSERT_FALSE(fakeEmitter->empty());
+    
+    fakeEmitter->publish(FakeEvent{});
     ASSERT_TRUE(called);
-}
-
-TEST_F(FakeEmitterFixture, on_empty) {
-    ASSERT_TRUE(fakeHandle->empty());
-    fakeHandle->on([](const auto &, auto &) {
-    });
-    ASSERT_FALSE(fakeHandle->empty());
-    fakeHandle->publish(FakeEvent{});
-    ASSERT_FALSE(fakeHandle->empty());
-}
-
-TEST_F(FakeEmitterFixture, on_clear) {
-    ASSERT_TRUE(fakeHandle->empty());
-    fakeHandle->on([](const auto &, auto &) { FAIL(); });
-    ASSERT_FALSE(fakeHandle->empty());
-    fakeHandle->clear();
-    ASSERT_TRUE(fakeHandle->empty());
+    
+    fakeEmitter->clear();
+    ASSERT_TRUE(fakeEmitter->empty());
 }
